c96b8356aa34be9af2fb397e923f7f324024141a
Jon <jdliauw@gmail.com>
2017-10-08 12:07:04 -0400

    Fully functioning scraper script, API needed

--- craigslist_auto_scraper.py
+++ craigslist_auto_scraper.py
@@ -3,6 +3,7 @@ Craigslist Auto Scraper
 Kablam!
 """
 
+import json
 import re
 import requests
 import time
@@ -42,89 +43,63 @@ class Listing:
         self.url = url
         self.vin = vin
 
-URLS = []
+def query_string_creation(parameters):
+    """
+    craiglists query string filters are in the format: "&<field>=<value>""
+    For example: &condition=10 represents a new condition
+    Multiple conditions and fields can be chosen, and are just appended in the same format
+    """
 
-CITIES = [# "atlanta",
-          # "austin",
-          # "boston",
-          # "chicago",
-          "dallas",
-          # "denver",
-          # "detroit",
-          # "houston",
-          # "lasvegas",
-          # "losangeles",
-          # "miami",
-          # "newyork",
-          # "orangecounty",
-          # "philadelphia",
-          # "phoenix",
-          # "portland",
-          # "raleigh",
-          # "sacramento",
-          # "sandiego",
-          # "seattle",
-          # "sfbay",
-    ]
+    query_string = ""
+    conditions    = {"new":10, "like new":20, "excellent":30, "good":40, "fair":50, "salvage":60}
+    cylinders     = {3:1, 4:2, 5:3, 6:4, 8:5, 10:6, 12:7}
+    drives        = {"fwd":1, "rwd":2, "4wd":3}
+    fuels         = {"gas":1, "diesel":2, "hybrid":3, "electric":4} # "other":5
+    colors        = {"black":1, "blue":2, "brown":20, "green":3, "grey":4, "orange":5, "purple":6,
+                     "red":7, "silver":8, "white":9, "yellow":10, "custom":11}
+    sizes         = {"compact":1, "full-size":2, "mid-size":3, "sub-compact":4}
+    titles        = {"clean":1, "salvage":2, "rebuilt":3, "parts only":4, "lien":5, "missing":6}
+    transmissions = {"manual":1, "automatic":2} # "other":3
+    types         = {"bus":1, "convertible":2, "coupe":3, "hatchback":4, "mini-van":5, "offroad":6,
+                     "pickup":7, "sedan":8, "truck":9, "SUV":10, "wagon":11, "van":12, "other":13}
 
-KEYWORDS = ["minibus",
-            "mercedes benz sprinter",
-            # "dodge promaster",
-            # "ford transit",
-            # "nissan nv",
-            # "minibus",
-]
+    # dictionary not hashable, so reversed so string:dict
+    fields = {"&condition=":conditions,
+              "&auto_cylinders=":cylinders,
+              "&auto_drivetrain=":drives,
+              "&auto_fuel_type=":fuels,
+              "&auto_paint=":colors,
+              "&auto_size=":sizes,
+              "&auto_title_status=":titles,
+              "&auto_transmission=":transmissions,
+              "&auto_bodytype=":types,
+              }
 
-"""
-    MULTIPLE (just add again)
+    for field in fields:
+        for field_value in fields[field]:
+            if str(field_value) in parameters:
+                query_string += "{0}{1}".format(field, fields[field][field_value])
+    return query_string
 
-    condition: &condition=
-    10=new, 20=like new, 30=excellent, 40=good, 50=fair, 60=salvage
-
-    cylinders: &auto_cylinders=
-    1=3,2=4,3=5,4=6,5=8,6=10,7=12
-
-    drive: &auto_drivetrain=
-    1=fwd,2=rwd,3=4wd 
-
-    fuel: &auto_fuel_type=
-    1=gas,2=diesel,3=hybrid,4=electric,5=other
-
-    color: &auto_paint=
-    1=black,2=blue,20=brown,3=green,4=grey,5=orange,6=purple,7=red,8=silver,9=white,10=yellow,11=custom
-
-    size: &auto_size=1
-    1=compact,2=full-size,3=mid-size,4=sub-compact
-
-    title_status: &auto_title_status=
-    1=clean,2=salvage,3=rebuilt,4=parts only,5=lien,6=missing
-
-    transmission: &auto_transmission=
-    1=manual,2=automatic,3=other
-
-    type: &auto_bodytype=
-    1=bus,2=convertible,3=coupe,4=hatchback,5=mini-van,6=offroad,7=pickup,8=sedan,9=truck,10=SUV,11=wagon,12=van,13=other
-
-"""
-
-def generate_start_urls():
+def generate_start_urls(cities, keywords, parameters):
     """Generate start URLs"""
     start_urls = []
-    for city in CITIES:
-        for keyword in KEYWORDS:
+
+    for city in cities:
+        for keyword in keywords:
+            # format for query string
             if " " in keyword:
-                kws = keyword.split(" ")
-                keyword = ""
-                for kw in kws:
-                    keyword += kw +  "+"
-                keyword = keyword[:-1]
-            start_urls.append("http://{0}.craigslist.org/search/sss?query={1}".format(city, keyword))
+                keyword = keyword.strip().replace(" ", "+")
+
+            query_string = query_string_creation(parameters)
+            start_urls.append("http://{0}.craigslist.org/search/sss?query={1}{2}".format(city, keyword, query_string))
     return start_urls
 
 def generate_individual_list_urls(start_urls):
     """Generate individual list URLs"""
     il_urls = []
     for start_url in start_urls:
+        time.sleep(1.23)
         page = requests.get(start_url)
         tree = html.fromstring(page.content)
         il_urls += tree.xpath("//a[@class='result-title hdrlnk']/@href")
@@ -137,9 +112,10 @@ def parse_page(url):
     spans = tree.xpath("//p[@class='attrgroup']/span")
 
     # return string instead of list and remove $
-    list_price = tree.xpath("//span[@class='price']/text()")[0][1:]
-    if list_price is None:
+    list_price = tree.xpath("//span[@class='price']/text()")
+    if list_price is None or list_price == []:
         return None
+    list_price = list_price[0][1:]
 
     vin = None
     condition = None
@@ -188,12 +164,13 @@ def parse_page(url):
                 if re.match(r'[0-9]{4}', field):
                     make = field
 
+    # naive, but just take location from URL
     start_index = url.find("//")
     end_index = url.find(".craigs")
     location = url[start_index + 2:end_index]
 
-    print (condition, cylinders, drive, fuel, list_price, location, make,
-           odometer, paint_color, size, title_status, transmission, url, vin)
+    # print (condition, cylinders, drive, fuel, list_price, location, make,
+    #        odometer, paint_color, size, title_status, transmission, url, vin)
 
     return Listing(condition, cylinders, drive, fuel, list_price, location, make, odometer,
                    paint_color, size, title_status, transmission, url, vin)
@@ -201,16 +178,66 @@ def parse_page(url):
 def run():
     """main"""
     listings = []
-    start_urls = generate_start_urls()
+
+    cities = [# "atlanta",
+              # "austin",
+              # "boston",
+              # "chicago",
+              "dallas",
+              "denver",
+              # "detroit",
+              # "houston",
+              # "lasvegas",
+              # "losangeles",
+              # "miami",
+              # "newyork",
+              # "orangecounty",
+              # "philadelphia",
+              # "phoenix",
+              # "portland",
+              # "raleigh",
+              # "sacramento",
+              # "sandiego",
+              # "seattle",
+              # "sfbay",
+        ]
+
+    keywords = ["mercedes benz sprinter",
+                # "minibus",
+                # "dodge promaster",
+                # "ford transit",
+                # "nissan nv",
+                # "minibus",
+    ]
+
+    parameters = "new,white,sedan,rwd,gas,clean,automatic"
+
+    # json from django
+    json_response = json.dumps(
+        {"cities":cities,
+         "keywords":keywords,
+         "parameters":"new"}
+    )
+    decoded_json = json.loads(json_response)
+    cities = decoded_json["cities"]
+    keywords = decoded_json["keywords"]
+    parameters = decoded_json["parameters"]
+
+    start_urls = generate_start_urls(cities, keywords, parameters)
+
+    for start_url in start_urls:
+        print "\n", start_url
     il_urls = generate_individual_list_urls(start_urls)
 
     for il_url in il_urls:
-        time.sleep(.75)
-        listings.append(parse_page(il_url))
+        # let's not get blacklisted
+        time.sleep(1.23)
+        listing = parse_page(il_url)
+        if listing is not None:
+            listings.append(listing)
 
-    # for listing in listings:
-    #     print listing
-
+    for listing in listings:
+        print listing.condition, listing.cylinders, listing.drive, listing.fuel, listing.list_price, listing.location, listing.make, listing.odometer, listing.paint_color, listing.size, listing.title_status, listing.transmission, listing.url, listing.vin
 
 if __name__ == "__main__":
     run()
